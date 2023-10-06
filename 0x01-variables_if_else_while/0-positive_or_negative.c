ec001a3e61ab23a097d08e537466de69e725506f
root <ayosikho@gmail.com>
2023-10-06 15:20:09 -0700

    is it great or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * main the eentry point
+ * assigning a random number to var n each time it is executed
+ * return: write
+ * */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+	return (0);
+}
