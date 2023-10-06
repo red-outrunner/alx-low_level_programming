1b25734aba0cd3417370289ecde83e98c8cff2ab
root <ayosikho@gmail.com>
2023-10-06 15:45:12 -0700

    print alphabet in l-case

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * a program that prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int alphabet;
+	
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		alphabet = tolower(alphabet);
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
