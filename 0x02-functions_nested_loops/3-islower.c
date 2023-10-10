225983f4d9d6230185bd47f3d9a3558399bbeca7
root <ayosikho@gmail.com>
2023-10-10 11:56:28 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _islowe - short description, single line
- * c: contains value to be compared
+ * _islower - short description, single line
+ * @c: contains value to be compared
  * Return: Always 0.
  */
 int _islower(int c)
