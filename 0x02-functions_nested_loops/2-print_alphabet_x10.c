ce53c23f9c56b1c4a84f9a81fa44d91f8ddc8c2e
root <ayosikho@gmail.com>
2023-10-10 09:13:21 -0700

    lwrcase alphabetsx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,20 @@
  *Return: Always 0
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	char alphabet;
+	int alphabet;
+	int count;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	count = 0;
+	while (count < 10)
 	{
-		_putchar(alphabet);
+		for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+
+		count++;
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
