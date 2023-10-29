fc101b95e61c6920a8a8e40ea12c55a4f44bdf6a
root <root@chillMaster>
2023-10-29 20:31:31 +0200

    solution

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,21 +1,15 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
+
 /**
- * _strlen - Calculate the length of a string
- * @s: Pointer to the string
- *
- * Return: The length of the string
+ * _strlen - returns the length of a string.
+ * @s: input stringeturn.
+ * Return: length of a string.
  */
 int _strlen(char *s)
 {
-	int len = 0;
-	if (s != NULL)
-	{
-		while (s[len] != '\0')
-		{
-			len++;
-		}
-	}
-	return (len);
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
 }
