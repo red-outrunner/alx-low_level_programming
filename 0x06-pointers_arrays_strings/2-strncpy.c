9a851c2a7822047eb201974cbfcaa787004abe46
root <root@chillMaster>
2023-11-03 15:56:13 +0200

    solution

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,12 +14,10 @@ char *_strncpy(char *dest, char *src, int n)
 	int byte_count;
 
 	for (byte_count = 0; byte_count < n && src[byte_count] != '\0'; byte_count++)
-	{
 		dest[byte_count] = src[byte_count];
-	}
+
 	for (; byte_count < n; byte_count++)
-	{
 			dest[byte_count] = '\0';
-	}
+	
 	return (dest);
 }
